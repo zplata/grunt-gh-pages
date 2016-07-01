8b6b3cbb2fac19704474381910471345fda1894f
Zach Plata <zach.plata@cerner.com>
2016-06-30 22:25:49 -0500

    Move changes

--- tasks/gh-pages.js
+++ tasks/gh-pages.js
@@ -7,7 +7,7 @@ var wrench = require('wrench');
 var pkg = require('../package.json');
 var git = require('../lib/git');
 
-var copy = require('../lib/util').copy;
+var util = require('../lib/util');
 
 function getCacheDir() {
   return path.join('.grunt', pkg.name);
@@ -16,23 +16,23 @@ function getCacheDir() {
 function getRemoteUrl(dir, remote) {
   var repo;
   return git(['config', '--get', 'remote.' + remote + '.url'], dir)
-      .progress(function(chunk) {
-        repo = String(chunk).split(/[\n\r]/).shift();
-      })
-      .then(function() {
-        if (repo) {
-          return Q.resolve(repo);
-        } else {
-          return Q.reject(new Error(
-              'Failed to get repo URL from options or current directory.'));
-        }
-      })
-      .fail(function(err) {
+    .progress(function(chunk) {
+      repo = String(chunk).split(/[\n\r]/).shift();
+    })
+    .then(function() {
+      if (repo) {
+        return Q.resolve(repo);
+      } else {
         return Q.reject(new Error(
-            'Failed to get remote.origin.url (task must either be run in a ' +
-            'git repository with a configured origin remote or must be ' +
-            'configured with the "repo" option).'));
-      });
+          'Failed to get repo URL from options or current directory.'));
+      }
+    })
+    .fail(function(err) {
+      return Q.reject(new Error(
+        'Failed to get remote.origin.url (task must either be run in a ' +
+        'git repository with a configured origin remote or must be ' +
+        'configured with the "repo" option).'));
+    });
 }
 
 function getRepo(options) {
@@ -74,6 +74,7 @@ module.exports = function(grunt) {
       remote: 'origin',
       base: process.cwd(),
       only: '.',
+      move: [],
       push: true,
       message: 'Updates',
       silent: false
@@ -119,111 +120,132 @@ module.exports = function(grunt) {
 
     var repoUrl;
     getRepo(options)
-        .then(function(repo) {
-          repoUrl = repo;
-          log('Cloning ' + urlSafe(repo,'[secure]') + ' into ' + options.clone);
-          return git.clone(repo, options.clone, options.branch, options);
-        })
-        .then(function() {
-          return getRemoteUrl(options.clone, options.remote)
-              .then(function(url) {
-                if (url !== repoUrl) {
-                  var message = 'Remote url mismatch.  Got "' + url + '" ' +
-                      'but expected "' + repoUrl + '" in ' + options.clone +
-                      '.  If you have changed your "repo" option, try ' +
-                      'running `grunt gh-pages-clean` first.';
-                  return Q.reject(new Error(message));
-                } else {
-                  return Q.resolve();
-                }
-              });
-        })
-        .then(function() {
-          // only required if someone mucks with the checkout between builds
-          log('Cleaning');
-          return git.clean(options.clone);
-        })
-        .then(function() {
-          log('Fetching ' + options.remote);
-          return git.fetch(options.remote, options.clone);
-        })
-        .then(function() {
-          log('Checking out ' + options.remote + '/' +
-              options.branch);
-          return git.checkout(options.remote, options.branch,
-              options.clone);
-        })
-        .then(function() {
-          if (!options.add) {
-            log('Removing files');
-            return git.rm(only.join(' '), options.clone);
-          } else {
-            return Q.resolve();
+      .then(function(repo) {
+        repoUrl = repo;
+        log('Cloning ' + urlSafe(repo,'[secure]') + ' into ' + options.clone);
+        return git.clone(repo, options.clone, options.branch, options);
+      })
+      .then(function() {
+        return getRemoteUrl(options.clone, options.remote)
+          .then(function(url) {
+            if (url !== repoUrl) {
+              var message = 'Remote url mismatch.  Got "' + url + '" ' +
+                'but expected "' + repoUrl + '" in ' + options.clone +
+                '.  If you have changed your "repo" option, try ' +
+                'running `grunt gh-pages-clean` first.';
+              return Q.reject(new Error(message));
+            } else {
+              return Q.resolve();
+            }
+          });
+      })
+      .then(function() {
+        // only required if someone mucks with the checkout between builds
+        log('Cleaning');
+        return git.clean(options.clone);
+      })
+      .then(function() {
+        log('Fetching ' + options.remote);
+        return git.fetch(options.remote, options.clone);
+      })
+      .then(function() {
+        log('Checking out ' + options.remote + '/' +
+          options.branch);
+        return git.checkout(options.remote, options.branch,
+          options.clone);
+      })
+      .then(function() {
+        if (!options.add) {
+          log('Removing files');
+          return git.rm(only.join(' '), options.clone);
+        } else {
+          return Q.resolve();
+        }
+      })
+      .then(function() {
+        log('Copying files');
+        return util.copy(files, options.base, options.clone);
+      })
+      .then(function() {
+        if(options.move) {
+          log('Moving files around');
+          if(!Array.isArray(options.move)) {
+            options.move = [options.move];
           }
-        })
-        .then(function() {
-          log('Copying files');
-          return copy(files, options.base, options.clone);
-        })
-        .then(function() {
-          log('Adding all');
-          return git.add('.', options.clone);
-        })
-        .then(function() {
-          if (options.user) {
-            return git(['config', 'user.email', options.user.email],
-                options.clone)
-                .then(function() {
-                  return git(['config', 'user.name', options.user.name],
-                      options.clone);
-                });
-          } else {
-            return Q.resolve();
-          }
-        })
-        .then(function() {
-          log('Committing');
-          return git.commit(options.message, options.clone);
-        })
-        .then(function() {
-          if (options.tag) {
-            log('Tagging');
-            var deferred = Q.defer();
-            git.tag(options.tag, options.clone)
-              .then(function() {
-                  return deferred.resolve();
-                })
-              .fail(function(error) {
-                  // tagging failed probably because this tag alredy exists
-                  log('Tagging failed, continuing');
-                  grunt.log.debug(error);
-                  return deferred.resolve();
-                });
-            return deferred.promise;
-          } else {
-            return Q.resolve();
-          }
-        })
-        .then(function() {
-          if (options.push) {
-            log('Pushing');
-            return git.push(options.remote, options.branch,
+          var moveFiles = [];
+          options.move.forEach(function(move) {
+            moveFiles.push({
+              files: grunt.file.expand({
+                filter: 'isFile',
+                cwd: path.join(options.clone, move.base),
+                dot: options.dotfiles
+              }, move.src),
+              base: path.join(options.clone, move.base),
+              dest: path.join(options.clone, move.dest)
+            })
+          });
+          return util.multiCopy(moveFiles, true);
+        }
+      })
+      .then(function() {
+        log('Adding all');
+        return git.add('.', options.clone);
+      })
+      .then(function() {
+        if (options.user) {
+          return git(['config', 'user.email', options.user.email],
+            options.clone)
+            .then(function() {
+              return git(['config', 'user.name', options.user.name],
                 options.clone);
-          } else {
-            return Q.resolve();
-          }
-        })
-        .then(function() {
-          done();
-        }, function(error) {
-          if (options.silent) {
-            error = new Error(
-                'Unspecified error (run without silent option for detail)');
-          }
-          done(error);
-        }, function(progress) {
-          grunt.verbose.writeln(progress);
-        });
+            });
+        } else {
+          return Q.resolve();
+        }
+      })
+      .then(function() {
+        log('Committing');
+        return git.commit(options.message, options.clone);
+      })
+      .then(function() {
+        if (options.tag) {
+          log('Tagging');
+          var deferred = Q.defer();
+          git.tag(options.tag, options.clone)
+            .then(function() {
+              return deferred.resolve();
+            })
+            .fail(function(error) {
+              // tagging failed probably because this tag alredy exists
+              log('Tagging failed, continuing');
+              grunt.log.debug(error);
+              return deferred.resolve();
+            });
+          return deferred.promise;
+        } else {
+          return Q.resolve();
+        }
+      })
+      .then(function() {
+        if (options.push) {
+          log('Pushing');
+          return git.push(options.remote, options.branch,
+            options.clone);
+        } else {
+          return Q.resolve();
+        }
+      })
+      .then(function() {
+        done();
+      }, function(error) {
+        if (options.silent) {
+          error = new Error(
+            'Unspecified error (run without silent option for detail)');
+        }
+        done(error);
+      }, function(progress) {
+        grunt.verbose.writeln(progress);
+      });
   });
 
   grunt.registerTask('gh-pages-clean', 'Clean cache dir', function() {
